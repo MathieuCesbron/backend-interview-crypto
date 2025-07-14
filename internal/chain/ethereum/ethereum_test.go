a185464755226ad3af3133be7c738ce9740a2e94
mathieu cesbron <mathieu.rudy.cesbron@gmail.com>
2025-07-14 19:43:36 +0800

    Add Ethereum tests

--- internal/chain/ethereum/ethereum_test.go
+++ internal/chain/ethereum/ethereum_test.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"encoding/json"
 	"math/big"
+	"os"
 	"testing"
 	"time"
 
@@ -13,23 +14,38 @@ import (
 	"github.com/segmentio/kafka-go"
 )
 
-type mockEthClient struct{}
+const (
+	addr1 = "0xb0CEe983642cA499A42FDf5c50882F42cDB3de15"
+	addr2 = "0x92a1F35B8df8B3f98cB41b5f115ed6A114257804"
+)
+
+type mockEthClient struct {
+	block uint64
+}
 
 func (m *mockEthClient) BlockNumber(ctx context.Context) (uint64, error) {
-	return 100, nil
+	m.block++
+	return m.block, nil
 }
 
 func (m *mockEthClient) BlockByNumber(ctx context.Context, number *big.Int) (*types.Block, error) {
-	to := common.HexToAddress("")
+	// from := common.HexToAddress(addr1)
+	to := common.HexToAddress(addr2)
 
 	tx := types.NewTransaction(
 		0, to, big.NewInt(1000000000000000000), 21000, big.NewInt(1000000000), nil,
 	)
 
-	block := types.NewBlock(
-		&types.Header{},
-		&types.Body{
-			Transactions: []*types.Transaction{tx}}, nil, nil)
+	// maybe because the block if fake ?
+	block := types.NewBlockWithHeader(
+		&types.Header{
+			Number: big.NewInt(500),
+		},
+	)
+
+	block = block.WithBody(types.Body{
+		Transactions: []*types.Transaction{tx}},
+	)
 
 	return block, nil
 }
@@ -39,6 +55,8 @@ func TestEthereumWatch(t *testing.T) {
 	kafkaChan := make(chan kafka.Message, 1)
 	e := NewEthereumWatcher(mockClient, kafkaChan)
 
+	to := common.HexToAddress(addr2)
+	os.Setenv("ETHEREUM_ADDRESSES", to.String())
 	go e.Watch()
 
 	select {
@@ -48,7 +66,7 @@ func TestEthereumWatch(t *testing.T) {
 		if err != nil {
 			t.Fatalf("Failed to decode Kafka message: %v", err)
 		}
-	case <-time.After(2 * time.Second):
+	case <-time.After(200 * time.Second):
 		t.Fatal("No Kafka message received")
 	}
 }
